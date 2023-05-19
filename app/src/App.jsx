a307afa5dbfcd94121a62189c1d9a4682c816e0a
George Leyva <leyvageorge@hotmail.com>
2023-05-18 21:21:23 -0700

    added more pages/componenets

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+//Pages
 import HomePage from './components/HomePage';
 import NormalTemplate from './components/NormalTemplate';
+import ErrorPage from './components/ErrorPage';
 
+//Style
+import './App.css';
 function App() {
   return (
       
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/home" element={<NormalTemplate/>} />
+        <Route path="*" element={ <ErrorPage/> } />
       </Routes>
     </Router>
   );
---
+++ app/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About () {
+    return(
+        <div>
+            <h2>
+                Web Developers
+            </h2>
+        </div>
+    )
+}
+
+export default About
