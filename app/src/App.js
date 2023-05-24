269f5024fd3b5f14806c6131c344f16aa939ad52
Fadi-Faiyad <zenfaiyad@gmail.com>
2023-05-23 19:49:11 -0700

    branch

--- app/src/App.js
+++ app/src/App.js
@@ -1,50 +1,74 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { useTable } from 'react-table';
-import { useState } from 'react';
 
 function App() {
-  const data = React.useMemo(() => [], []);
-  const columns = React.useMemo(() => [
-    {
-      Header: 'WriteList'
-    },
-    {
-      Header: 'TodoList'
-    },
-    {
-      Header: 'StartedList'
-    },
-    {
-      Header: 'ReviewList'
-    },
-    {
-      Header: 'FinshedList'
-    },
-  ])
-  const table = useTable({ columns, data });
+  const [input, setInput] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [progressList, setProgressList] = useState([]);
+  const [completedList, setCompletedList] = useState([]);
+
+  const handleClick = () => {
+    const id = todoList.length + 1;
+    const newTask = {
+      id: id,
+      task: input,
+      complete: false,
+    };
+    setTodoList(prev => [...prev, newTask]);
+    setInput('');
+  };
+
+  const handleMoveToProgress = id => {
+    const task = todoList.find(task => task.id === id);
+    if (task) {
+      setProgressList(prev => [...prev, task]);
+      setTodoList(prev => prev.filter(task => task.id !== id));
+    }
+  };
+
+  const handleMoveToCompleted = id => {
+    const task = progressList.find(task => task.id === id);
+    if (task) {
+      setCompletedList(prev => [...prev, task]);
+      setProgressList(prev => prev.filter(task => task.id !== id));
+    }
+  };
 
   return (
     <div className="App">
-      <div className='container'>
-        <table {...getTableProps()}>
-        <thead>    //the header body
-          {headerGroups.map((headerGroup) => ( //this array of objecat for each header groups
-            <tr {...headerGroup.getHeaderGroupProps()}>
-               {headerGroup.headers.map((column) => (
-                <th {...column.getTableProps()}>
-                  {cplumn.render('Header')}
-                </th>
-               ))}
-
-            </tr>
-          ))}
-          </thead>
-          <tbody>   
-
-          </tbody>
-        </table>
+      <div className="lists-container">
+        <div className="todo-list">
+          <h2>Todo List</h2>
+          <input value={input} onChange={e => setInput(e.target.value)} />
+          <button onClick={handleClick}>Add</button>
+          <ul id='todoList'>
+            {todoList.map(todo => (
+              <li key={todo.id}>
+                {todo.task}
+                <button onClick={() => handleMoveToProgress(todo.id)}>Move to Progress</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="progress-list">
+          <h2>Progress List</h2>
+          <ul id='progress'>
+            {progressList.map(task => (
+              <li key={task.id}>
+                {task.task}
+                <button onClick={() => handleMoveToCompleted(task.id)}>Move to Completed</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="completed-list">
+          <h2>Completed List</h2>
+          <ul id='completed'>
+            {completedList.map(task => (
+              <li key={task.id}>{task.task}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
