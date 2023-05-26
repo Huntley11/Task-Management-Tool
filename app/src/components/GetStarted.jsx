55bf30ce45fb5264d9a2283b073b9350d327f06a
George Leyva <leyvageorge@hotmail.com>
2023-05-25 17:55:13 -0700

    changes to getting started

--- app/src/components/GetStarted.jsx
+++ app/src/components/GetStarted.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "./Navigation";
 import Carousel from "react-bootstrap/Carousel";
-import Footer from "./Footer";
+
 
 function GetStarted() {
     return (
